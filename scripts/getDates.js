f6da64de3a3c79f758bc5ea96f27bfd65634f99b
stburgess <stephentburgess@aim.com>
2024-03-07 22:01:31 +0200

    initial home page plus some other stuff

---
+++ scripts/getDates.js
@@ -0,0 +1,2 @@
+document.querySelector('#copyright').textContent += `${new Date().getFullYear()} - Stephen T. Burgess - Bulgaria`;
+document.querySelector('#lastModified').textContent = `Last modified: ${document.lastModified}`;
